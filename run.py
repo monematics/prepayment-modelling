2fc08a98684113365f25e4e1f8adc694feb38890
leo zhang <leo.zhang927@gmail.com>
2024-02-02 03:06:37 -0500

    saving wip

---
+++ run.py
@@ -0,0 +1,16 @@
+#RAW_DATA_PATH =
+def run():
+    """
+    ideally we should upload to cloud to run
+    # step 1 : clean and prep the entire dataset
+    # step 2 : train test validate and evaluate and pick the best model out of all candidate models
+                full cycle deep learning vs XGBoost, random forest, tree method etc
+    # step 3 : performance testing
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    run()
+
+
